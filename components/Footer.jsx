cb6dc9afe63153052e0bc564417306aedd08bd6e
Bostonoff <muhammadboston1101@gmail.com>
2024-08-22 14:58:58 +0500

    see what happens

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,17 +1,15 @@
 import { AiFillCopyrightCircle } from "react-icons/ai";
 import { MdMail } from "react-icons/md";
 
-
 const Footer = () => {
   return (
-    <div id="intro" className="mt-4 mb-10 lg:mb-0">
+    <div className="mt-4 mb-10 lg:mb-0">
       <div className="flex h-10 items-center justify-between text-xs font-normal text-LightGray py-4 px-2 md:px-4 w-full bg-MidNightBlack">
         <div className="flex items-center">
           <div className="mr-1 text-base">
             <AiFillCopyrightCircle />
           </div>
           <span>2024 All Rights Reserved.</span>
-      
         </div>
         <div className="flex items-center">
           <a
@@ -21,7 +19,7 @@ const Footer = () => {
             Made with ❤️ by <span className="font-bold">Bostonoff</span>
           </a>
         </div>
-        <div className="hidden md:flex items-center">
+        <div className="block md:flex items-center">
           <div className="mr-1 text-base">
             <MdMail />
           </div>
